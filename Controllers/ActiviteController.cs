7e3c6792312001523ab1a229048cdea23679db43
gabri <gabri@DESKTOP-5BJVM3V>
2021-12-06 13:10:49 -0500

    Add project files.

---
+++ Controllers/ActiviteController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using GestionActivités.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using GestionActivités.Models.Activités;
+
+namespace GestionActivités.Controllers
+{
+    public class ActiviteController : Controller
+    {
+        private string connectionString;
+        public IConfiguration configuration;
+
+        public ActiviteController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+        // GET: ActiviteController
+        public ActionResult Index()
+        {
+            List<Activite> listeActivites;
+
+            connectionString = configuration.GetConnectionString("defaultConnection");
+            SqlCommand cmd = new SqlCommand
+            {
+                CommandType = CommandType.StoredProcedure,
+                CommandText = "getActivite"
+            };
+            SqlConnection conn = new SqlConnection(connectionString);
+            cmd.Connection = conn;
+
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            listeActivites = new List<Activite>();
+            while (reader.Read())
+            {
+                Activite a = new Activite();
+                a.nomActivite = reader.GetString("nomActivite");
+                a.quand = reader.GetDateTime("quand");
+                a.duree = reader.GetFloat("duree");
+                listeActivites.Add(a);
+            }
+            return View(listeActivites);
+        }
+
+        // GET: HomeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Activités/Activite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionActivités.Models.Activités
+{
+    public class Activite
+    {
+        public string nomActivite { get; set; }
+        public float duree { get; set; }
+        public DateTime quand { get; set; }
+        public double prix { get; set; }
+        public int vote { get; set; }
+        public int covoiturage { get; set; }
+
+        public Activite(string nomActivite, float duree, DateTime quand, double prix)
+        {
+            this.nomActivite = nomActivite;
+            this.duree = duree;
+            this.quand = quand;
+            this.prix = prix;
+        }
+        public Activite()
+        {
+
+        }
+    }
+}
