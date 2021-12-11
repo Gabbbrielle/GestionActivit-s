96ad8e32c8bc4f17211d290002252b12792927bf
Gabrielle Bouchard <gabrielle.bouchar@gmail.com>
2021-12-11 12:55:57 -0500

    Début de la solution pour le TP des activités

---
+++ Models/Participants/Participants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionActivités.Models.Participants
+{
+    public class Participants
+    {
+        public string Prénom { get; set; }
+        public string Nom { get; set; }
+        public int Age { get; set; }
+        public int Voiture { get; set; }
+        public int PlaceVoiture { get; set; }
+        public string PickUp { get; set; }
+
+        public Participants(string prénom, string nom, int age, int voiture, int placeVoiture, string pickUp)
+        {
+            Prénom = prénom;
+            Nom = nom;
+            Age = age;
+            Voiture = voiture;
+            PlaceVoiture = placeVoiture;
+            PickUp = pickUp;
+        }
+        public Participants()
+        {
+
+        }
+    }
+}
