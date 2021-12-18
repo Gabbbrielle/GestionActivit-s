c2914528916a1ca2eafb2e8141b3f115342cec8c
Gabrielle Bouchard <gabrielle.bouchar@gmail.com>
2021-12-17 22:24:08 -0500

    final maybe

--- Models/Activités/Activite.cs
+++ Models/Activités/Activite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,15 @@ namespace GestionActivités.Models.Activites
 {
     public class Activite
     {
+        
         public int id { get; set; }
         public string nomActivite { get; set; }
         public string emplacement { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMMM yyyy}")]
         public DateTime dateActivite { get; set; }
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
         public DateTime debut { get; set; }
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
         public DateTime fin { get; set; }
 
         public double prix { get; set; }
